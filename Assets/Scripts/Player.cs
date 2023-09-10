b62e81547fcb5af0ba20e831f00622888f531338
Lucas Vittor <lvvittor@gmail.com>
2023-09-10 20:41:13 -0300

    feat: add collision detection for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,57 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float playerRadius = 0.7f;
+    [SerializeField] private float playerHeight = 2f;
     [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = CalculateValidMoveDirection(inputVector);
 
+        MovePlayer(moveDir);
+    }
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-
+    private void MovePlayer(Vector3 moveDir){
+        float moveDistance = moveSpeed * Time.deltaTime;
+        transform.position += moveDir * moveDistance;
         isWalking = moveDir != Vector3.zero;
+        
+        UpdateRotation(moveDir);
+    }
 
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    private void UpdateRotation(Vector3 moveDir){
+        float rotateDistance = rotateSpeed * Time.deltaTime;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateDistance);
+    }
+
+    private bool CanMoveInDirection(Vector3 moveDir, float moveDistance){
+        return !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+    }
+
+    private Vector3 CalculateValidMoveDirection(Vector2 inputVector){
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        float moveDistance = moveSpeed * Time.deltaTime;
+
+        if (!CanMoveInDirection(moveDir, moveDistance)){
+            // Cannot move towards moveDir
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+
+            if (CanMoveInDirection(moveDirX, moveDistance)){
+                // Can move only on the X
+                moveDir = moveDirX;
+            } else {
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                if (CanMoveInDirection(moveDirZ, moveDistance)){
+                    // Can move only on the Z
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        return moveDir;
     }
 
     public bool IsWalking() {
