5bbd484a60c4bf347bf90e6582c2776833035a1c
festevezz <festevez@itba.edu.ar>
2023-10-04 20:09:35 -0300

    feat: add gui and recipe delivery

---
+++ Assets/Scripts/Visuals/DeliveryManagerUI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DeliveryManagerUI : MonoBehaviour {
+    [SerializeField] private Transform container;
+    [SerializeField] private Transform recipeTemplate;
+
+    private void Awake() {
+        recipeTemplate.gameObject.SetActive(false);
+
+    }
+
+    private void Start() {
+        DeliveryManager.Instance.OnRecipeSpawned += DeliveryManager_OnRecipeSpawned;
+        DeliveryManager.Instance.OnRecipeDelivered += DeliveryManager_OnRecipeDelivered;
+
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeDelivered(object sender, System.EventArgs e) {
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeSpawned(object sender, System.EventArgs e) {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual() {
+        foreach (Transform child in container) {
+            if (child == recipeTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        DeliveryManager.Instance.GetWaitingRecipeSOList().ForEach(recipe => {
+            Transform recipeTransform = Instantiate(recipeTemplate, container);
+            recipeTransform.gameObject.SetActive(true);
+            recipeTransform.GetComponent<DeliveryManagerSingleUI>().SetRecipeSO(recipe);
+        });
+    }
+}
