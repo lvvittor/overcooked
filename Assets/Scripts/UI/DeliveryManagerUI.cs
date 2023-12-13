65e704e561bf41235db84eb3b23037031f9209a9
festevezz <festevez@itba.edu.ar>
2023-12-13 20:59:56 -0300

    feat: add recipe timers logic

--- Assets/Scripts/UI/DeliveryManagerUI.cs
+++ Assets/Scripts/UI/DeliveryManagerUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static DeliveryManager;
 
 public class DeliveryManagerUI : MonoBehaviour {
     [SerializeField] private Transform container;
@@ -15,28 +16,42 @@ public class DeliveryManagerUI : MonoBehaviour {
     private void Start() {
         DeliveryManager.Instance.OnRecipeSpawned += DeliveryManager_OnRecipeSpawned;
         DeliveryManager.Instance.OnRecipeDelivered += DeliveryManager_OnRecipeDelivered;
+        DeliveryManager.Instance.OnRecipeTimeout += DeliveryManager_OnRecipeTimeout;
 
-        UpdateVisual();
+        UpdateVisual(new List<float>());
     }
 
-    private void DeliveryManager_OnRecipeDelivered(object sender, System.EventArgs e) {
-        UpdateVisual();
+    private void DeliveryManager_OnRecipeDelivered(object sender, RecipeEventArgs e) {
+        UpdateVisual(e.Timers);
     }
 
-    private void DeliveryManager_OnRecipeSpawned(object sender, System.EventArgs e) {
-        UpdateVisual();
+    private void DeliveryManager_OnRecipeSpawned(object sender, RecipeEventArgs e) {
+        UpdateVisual(e.Timers);
     }
 
-    private void UpdateVisual() {
+    private void DeliveryManager_OnRecipeTimeout(object sender, RecipeEventArgs e) {
+        UpdateVisual(e.Timers);
+    }
+
+    private void UpdateVisual(List<float> timers) {
         foreach (Transform child in container) {
             if (child == recipeTemplate) continue;
             Destroy(child.gameObject);
         }
 
-        DeliveryManager.Instance.GetWaitingRecipeSOList().ForEach(recipe => {
+        List<RecipeSO> waitingRecipes = DeliveryManager.Instance.GetWaitingRecipeSOList();
+        for (int i = 0; i < waitingRecipes.Count; i++)
+        {
             Transform recipeTransform = Instantiate(recipeTemplate, container);
             recipeTransform.gameObject.SetActive(true);
-            recipeTransform.GetComponent<DeliveryManagerSingleUI>().SetRecipeSO(recipe);
-        });
+            DeliveryManagerSingleUI singleUI = recipeTransform.GetComponent<DeliveryManagerSingleUI>();
+            singleUI.SetRecipeSO(waitingRecipes[i]);
+
+            // Check if there is a timer for this recipe
+            if (i < timers.Count)
+            {
+                singleUI.SetTimer(timers[i]);
+            }
+        }
     }
 }
